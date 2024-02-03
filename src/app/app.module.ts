f413d38f0dbeef94516a426ce5ac98f98c34b420
Augusto Gomez <agomezsantos95@gmail.com>
2024-02-03 16:23:16 -0300

    Add CoinListComponent to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { CoinListModule } from './coin-list/coin-list.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, CoinListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/coin-list/coin-list/coin-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CoinService } from 'src/app/core/coin.service';
+import { CoinStructure } from 'src/app/types';
+
+@Component({
+  selector: 'cr-coin-list',
+  templateUrl: './coin-list.component.html',
+  styleUrls: ['./coin-list.component.css'],
+})
+export class CoinListComponent implements OnInit {
+  public coins: CoinStructure[] = [];
+
+  constructor(private coinService: CoinService) {}
+
+  ngOnInit(): void {
+    this.coinService.coins$.subscribe((coins) => {
+      this.coins = coins;
+    });
+
+    this.coinService.loadCoins();
+  }
+}
