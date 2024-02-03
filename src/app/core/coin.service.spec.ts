890abd23f1deaee2b4a85a6f7c34e3209663bb55
Augusto Gomez <agomezsantos95@gmail.com>
2024-02-03 15:20:31 -0300

    Add testing suite for service

--- src/app/core/coin.service.spec.ts
+++ src/app/core/coin.service.spec.ts
@@ -1,16 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CoinService } from './coin.service';
+import { coins } from '../data/coins';
 
-describe('CoinService', () => {
+describe('Given a CoinService', () => {
   let service: CoinService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [CoinService] });
     service = TestBed.inject(CoinService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('When it calls its loadCharacters method', () => {
+    it('Then it should change the coins$ variable', () => {
+      service.loadCharacters();
+      expect(service.coins$.getValue()).toBe(coins);
+    });
   });
 });
