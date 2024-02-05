b35066ec79cf88e4271a25056f98cb5a0a921eff
Augusto Gomez <agomezsantos95@gmail.com>
2024-02-05 13:18:45 -0300

    Add CoinComponent testing suite

---
+++ src/app/mocks/coinMock.ts
@@ -0,0 +1,10 @@
+import { CoinStructure } from '../types';
+
+export const coinMock: CoinStructure = {
+  id: 'bitcoin',
+  name: 'btc',
+  symbol: 'Bitcoin',
+  image:
+    'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400',
+  currentPrice: 43352,
+};
