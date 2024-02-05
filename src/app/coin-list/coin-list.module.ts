127c998f22665e1b6d6934408c321f60e94974c4
Augusto Gomez <agomezsantos95@gmail.com>
2024-02-05 11:48:30 -0300

    Add CapitalizePipe to project

--- src/app/coin-list/coin-list.module.ts
+++ src/app/coin-list/coin-list.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { CoinListComponent } from './coin-list/coin-list.component';
 import { RouterModule } from '@angular/router';
 import { CoinComponent } from './coin/coin.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CoinListComponent, CoinComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SharedModule],
   exports: [CoinListComponent],
 })
 export class CoinListModule {}
