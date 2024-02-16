b9987f287a673f146eab363a3a7b9456315680f7
Augusto Gomez <agomezsantos95@gmail.com>
2024-02-16 15:10:18 -0300

    Add service for coin detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoinListComponent } from './coin-list/coin-list/coin-list.component';
+import { CoinDetailComponent } from './coin-detail/coin-detail/coin-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/coins', pathMatch: 'full' },
   { path: 'coins', component: CoinListComponent },
+  { path: 'coins/:coinId', component: CoinDetailComponent },
 ];
 
 @NgModule({
